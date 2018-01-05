28ccaf7f23616d0bbe4200cc89355127024ecb32
Jerry Wang <j3rry.wan9@gmail.com>
2018-01-05 15:10:42 -0800

    create project skeleton based on Spring Boot

---
+++ src/main/java/me/jerrywang/java/springdatademo/SpringDataDemo.java
@@ -0,0 +1,11 @@
+package me.jerrywang.java.springdatademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataDemo {
+    public static void main(final String[] args) {
+      SpringApplication.run(SpringDataDemo.class, args);
+    }
+}
