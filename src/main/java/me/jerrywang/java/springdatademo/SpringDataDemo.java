23db6a1d04a8afd459ae50e2e45a3ff43b7358a3
Jerry Wang <j3rry.wan9@gmail.com>
2018-04-19 10:45:33 -0700

    fix indentation

--- src/main/java/me/jerrywang/java/springdatademo/SpringDataDemo.java
+++ src/main/java/me/jerrywang/java/springdatademo/SpringDataDemo.java
@@ -10,16 +10,16 @@ import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringDataDemo {
-    public static void main(final String[] args) {
-      final ApplicationContext context = SpringApplication.run(SpringDataDemo.class, args);
+  public static void main(final String[] args) {
+    final ApplicationContext context = SpringApplication.run(SpringDataDemo.class, args);
 
-      final ProductRepository productRepository = context.getBean(ProductRepository.class);
-      final Product newProduct = new Product();
-      newProduct.setId(1L);
-      newProduct.setName("XPS 13");
-      newProduct.setUnitPrice(new BigDecimal(1300));
-      newProduct.setManufacturer("DELL");
-      newProduct.setCategory("Laptop");
-      productRepository.save(newProduct);
-    }
+    final ProductRepository productRepository = context.getBean(ProductRepository.class);
+    final Product newProduct = new Product();
+    newProduct.setId(1L);
+    newProduct.setName("XPS 13");
+    newProduct.setUnitPrice(new BigDecimal(1300));
+    newProduct.setManufacturer("DELL");
+    newProduct.setCategory("Laptop");
+    productRepository.save(newProduct);
+  }
 }
