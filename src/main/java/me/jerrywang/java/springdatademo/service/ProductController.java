5e0431343873c76af89371fe561d6928640fa042
Jerry Wang <j3rry.wan9@gmail.com>
2018-08-12 23:55:00 -0700

    use BasePathAwareController
    src/main/resources/application.properties

--- src/main/java/me/jerrywang/java/springdatademo/service/ProductController.java
+++ src/main/java/me/jerrywang/java/springdatademo/service/ProductController.java
@@ -1,12 +1,12 @@
-package me.jerrywang.java.springdatademo.controller;
+package me.jerrywang.java.springdatademo.service;
 
 import me.jerrywang.java.springdatademo.data.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.data.rest.webmvc.BasePathAwareController;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@BasePathAwareController
 public class ProductController {
   @Autowired
   private ProductRepository productRepository;
