2b2f27bfb876e7ebfdfe3dfd47b083c632e1fdc3
Jerry Wang <j3rry.wan9@gmail.com>
2018-08-15 21:19:40 -0700

    add a unit test based on REST Assured

---
+++ src/test/java/me/jerrywang/java/springdatademo/ListProductsTests.java
@@ -0,0 +1,18 @@
+package me.jerrywang.java.springdatademo;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import org.junit.Test;
+
+public class ListProductsTests {
+  @Test
+  public void listProductById() {
+    given()
+        .when()
+        .get("http://localhost:8080/api/v1/products/1")
+        .then()
+        .statusCode(200)
+        .assertThat()
+        .body("manufacturer", equalTo("DELL"));
+  }
+}
