51c32f1c74c2afae948c96f9565b65850adef093
Jerry Wang <j3rry.wan9@gmail.com>
2018-10-03 15:32:55 -0700

    use RestController

--- src/main/java/me/jerrywang/java/springdatademo/service/ProductController.java
+++ src/main/java/me/jerrywang/java/springdatademo/service/ProductController.java
@@ -1,19 +1,31 @@
 package me.jerrywang.java.springdatademo.service;
 
+import java.util.List;
+
 import me.jerrywang.java.springdatademo.data.ProductRepository;
+import me.jerrywang.java.springdatademo.domain.Product;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.BasePathAwareController;
-import org.springframework.ui.Model;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @BasePathAwareController
+@RestController
 public class ProductController {
+
   @Autowired
   private ProductRepository productRepository;
 
   @GetMapping("/products")
-  public String listAllProducts(final Model model) {
-    model.addAttribute("products", productRepository.findAll());
-    return "products";
+  public ResponseEntity<List<Product>> listAllProducts() {
+    final List<Product> products = productRepository.findAll();
+    return wrap(products, HttpStatus.OK);
+  }
+
+  private ResponseEntity<List<Product>> wrap(final List<Product> products, final HttpStatus responseStatus) {
+    return new ResponseEntity<>(products, responseStatus);
   }
 }
