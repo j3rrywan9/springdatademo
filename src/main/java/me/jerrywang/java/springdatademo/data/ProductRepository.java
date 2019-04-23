44e95a3a316b3dedeeefb3b9ae4aa943e85fa6f6
Jerry Wang <j3rry.wan9@gmail.com>
2019-04-23 16:12:16 -0700

    add an example to expose the search resource

--- src/main/java/me/jerrywang/java/springdatademo/data/ProductRepository.java
+++ src/main/java/me/jerrywang/java/springdatademo/data/ProductRepository.java
@@ -1,9 +1,13 @@
 package me.jerrywang.java.springdatademo.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import me.jerrywang.java.springdatademo.domain.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+  @Transactional(readOnly = true)
+  Long countById(@Param("id") Long id);
 }
