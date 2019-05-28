046fa62f6c1ce991daf177613b062393b33a3a8c
Jerry Wang <j3rry.wan9@gmail.com>
2019-05-28 12:26:48 -0700

    bump to Spring Boot 2.1.4.RELEASE

--- src/main/java/me/jerrywang/java/springdatademo/data/config/RepositoryConfig.java
+++ src/main/java/me/jerrywang/java/springdatademo/data/config/RepositoryConfig.java
@@ -2,12 +2,12 @@ package me.jerrywang.java.springdatademo.data.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
-import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
 import me.jerrywang.java.springdatademo.domain.Product;
 
 @Configuration
-public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
+public class RepositoryConfig implements RepositoryRestConfigurer {
 
   @Override
   public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
